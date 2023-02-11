221dc36eac4f65cd59cd9c6f3854f4cd3b661d73
dwitinpatel06 <dwitinpatel06@gmail.com>
2023-02-11 15:01:33 -0500

    Practice

---
+++ Practice/src/practice/Practice.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package practice;
+import java.util.Scanner;
+/**
+ *
+ * @author $ Dwiti Patel
+ */
+public class Practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+   
+   
+        String word = in.next();
+         
+        char[] reverse = new char[word.length()];
+        for(int i=0;i<=word.length();i++)
+        {
+         reverse[i] = word.charAt(i);
+    }
+        for(int i=reverse.length-1;i>=0;i--)
+        {
+         System.out.print(reverse[i]);
+    
+}
+        // TODO code application logic here
+    }
+
+}
